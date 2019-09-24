5e971d62fdc174450642195150be5586f86e20b0
Jake Rowell <letssplitanatom@gmail.com>
2019-09-24 07:02:49 -0500

    Optimized some of the auton functions, changed drivetrain to two motors and one strafe, started a strafe function for auton

--- include/user/drivetrain.hpp
+++ include/user/drivetrain.hpp
@@ -10,13 +10,21 @@ void drivetrainInit(std::string breakMode);
 //Args for side are a lowercase 'l', and 'r'
 int getAvgDriveSideTicks(char side);
 
-//Set the arbitrary "power" of one side of the drivetrainInit
+//Set the arbitrary "power" of one side of the drivetrain
 //Args for side are a lowercase 'l', and 'r', power is from -127 to 127
 void setDriveSidePower(int power, char side);
 
-//Set the arbitrary "power" of one side of the drivetrainInit
-//Args for side are a lowercase 'l', and 'r', power is from -200 to 200
+//Set the arbitrary "power" of the strafe motor on the drivetrain
+//Args for power is from -127 to 127
+void setStrafePower(int power);
+
+//Set the velocity target of one side of the drivetrain
+//Args for side are a lowercase 'l', and 'r', vel is from -200 to 200
 void setDriveSideVel(int vel, char side);
+
+//Set the velocity target for the strafe motor on the drivetrain
+//Args for vel is from -200 to 200
+void setStrafeVel(int vel);
 
 //Stop all the motors on the drivetrain
 void stopAll();
@@ -24,10 +32,14 @@ void stopAll();
 //Let the user control the drivetrain in driver control
 void controlDrivetrain(Controller controller);
 
-//Move the drivetrain a number of inches
+//Move the drivetrain forward or back a number of inches
 //Args for direction are "forward" and "backward"
-void driveDistance(int inches, std::string direction);
+void driveInches(int inches, std::string direction);
 
 //Turn the drivetrain a number of turnDegrees
 //Args for direction are "left" with a lowercase "l", and "right"
 void turnDegrees(int degrees, std::string direction);
+
+//Strafe the drivetrain left or right a number of inches
+//Arags for direction are a lowercase 'l', and 'r'
+void strafeInches(int inches, char direction);
--- src/user/drivetrain.cpp
+++ src/user/drivetrain.cpp
@@ -1,37 +1,42 @@
 #include "user/drivetrain.hpp"
 
-Motor FR(19, E_MOTOR_GEARSET_18, 1, E_MOTOR_ENCODER_DEGREES);
-Motor FL(15, E_MOTOR_GEARSET_18, 0, E_MOTOR_ENCODER_DEGREES);
+//Motor FR(19, E_MOTOR_GEARSET_18, 1, E_MOTOR_ENCODER_DEGREES);
+//Motor FL(15, E_MOTOR_GEARSET_18, 0, E_MOTOR_ENCODER_DEGREES);
 Motor BR(17, E_MOTOR_GEARSET_18, 1, E_MOTOR_ENCODER_DEGREES);
 Motor BL(14, E_MOTOR_GEARSET_18, 0, E_MOTOR_ENCODER_DEGREES);
+Motor strafe(15, E_MOTOR_GEARSET_36, E_MOTOR_ENCODER_DEGREES);
 
 void drivetrainInit(std::string brakeMode)
 {
   if(brakeMode == "coast")
   {
-    FR.set_brake_mode(E_MOTOR_BRAKE_COAST);
-    FL.set_brake_mode(E_MOTOR_BRAKE_COAST);
+    //FR.set_brake_mode(E_MOTOR_BRAKE_COAST);
+    //FL.set_brake_mode(E_MOTOR_BRAKE_COAST);
     BR.set_brake_mode(E_MOTOR_BRAKE_COAST);
     BL.set_brake_mode(E_MOTOR_BRAKE_COAST);
+    strafe.set_brake_mode(E_MOTOR_BRAKE_COAST);
   }
   else if(brakeMode == "brake")
   {
-    FR.set_brake_mode(E_MOTOR_BRAKE_BRAKE);
-    FL.set_brake_mode(E_MOTOR_BRAKE_BRAKE);
+    //FR.set_brake_mode(E_MOTOR_BRAKE_BRAKE);
+    //FL.set_brake_mode(E_MOTOR_BRAKE_BRAKE);
     BR.set_brake_mode(E_MOTOR_BRAKE_BRAKE);
     BL.set_brake_mode(E_MOTOR_BRAKE_BRAKE);
+    strafe.set_brake_mode(E_MOTOR_BRAKE_BRAKE);
   }
   else if(brakeMode == "hold")
   {
-    FR.set_brake_mode(E_MOTOR_BRAKE_HOLD);
-    FL.set_brake_mode(E_MOTOR_BRAKE_HOLD);
+    //FR.set_brake_mode(E_MOTOR_BRAKE_HOLD);
+    //FL.set_brake_mode(E_MOTOR_BRAKE_HOLD);
     BR.set_brake_mode(E_MOTOR_BRAKE_HOLD);
     BL.set_brake_mode(E_MOTOR_BRAKE_HOLD);
+    strafe.set_brake_mode(E_MOTOR_BRAKE_HOLD);
   }
-  FR.tare_position();
-  FL.tare_position();
+  //FR.tare_position();
+  //FL.tare_position();
   BR.tare_position();
   BL.tare_position();
+  strafe.tare_position();
 }
 
 int getAvgDriveSideTicks(char side)
@@ -39,56 +44,73 @@ int getAvgDriveSideTicks(char side)
   int avg = 0;
   if(side == 'l')
   {
-    avg = (FL.get_position() + BL.get_position())/2;
+    avg = /*(FL.get_position() + */BL.get_position();//)/2;
   }
   else if(side == 'r')
   {
-    avg = (FR.get_position() + BR.get_position())/2;
+    avg = /*(FR.get_position() + */BR.get_position();//)/2;
   }
   return avg;
+}
+
+int getStrafeTicks()
+{
+  return strafe.get_position(); //- is right, + is left
 }
 
 void setDriveSidePower(int power, char side)
 {
   if(side == 'r')
   {
-    FR.move(power);
+    //FR.move(power);
     BR.move(power);
   }
   else if(side == 'l')
   {
-    FL.move(power);
+    //FL.move(power);
     BL.move(power);
   }
+}
+
+void setStrafePower(int power)
+{
+  strafe.move(power);
 }
 
 void setDriveSideVel(int vel, char side)
 {
   if(side == 'r')
   {
-    FR.move_velocity(vel);
+    //FR.move_velocity(vel);
     BR.move_velocity(vel);
   }
   else if(side == 'l')
   {
-    FL.move_velocity(vel);
+    //FL.move_velocity(vel);
     BL.move_velocity(vel);
   }
 }
 
+void setStrafeVel(int vel)
+{
+  strafe.move_velocity(vel);
+}
+
 void stopAll()
 {
-  FR.move_velocity(0);
-  FL.move_velocity(0);
+  //FR.move_velocity(0);
+  //FL.move_velocity(0);
   BR.move_velocity(0);
   BL.move_velocity(0);
+  strafe.move_velocity(0);
 }
 
 void controlDrivetrain(Controller controller)
 {
   int lPower = controller.get_analog(E_CONTROLLER_ANALOG_RIGHT_Y) + controller.get_analog(E_CONTROLLER_ANALOG_RIGHT_X);
   int rPower = controller.get_analog(E_CONTROLLER_ANALOG_RIGHT_Y) - controller.get_analog(E_CONTROLLER_ANALOG_RIGHT_X);
-  if(abs(controller.get_analog(E_CONTROLLER_ANALOG_RIGHT_Y)) < 0 && abs(controller.get_analog(E_CONTROLLER_ANALOG_RIGHT_X)) < 0)
+  int sPower = controller.get_analog(E_CONTROLLER_ANALOG_LEFT_X);
+  if(abs(controller.get_analog(E_CONTROLLER_ANALOG_RIGHT_Y)) < 5 && abs(controller.get_analog(E_CONTROLLER_ANALOG_RIGHT_X)) < 5 && abs(sPower) < 5)
   {
     stopAll();
   }
@@ -96,13 +118,14 @@ void controlDrivetrain(Controller controller)
   {
     setDriveSidePower(lPower, 'l');
     setDriveSidePower(rPower, 'r');
+    setStrafePower(sPower);
   }
 }
 
-void driveDistance(int inches, std::string direction)
+void driveInches(int inches, std::string direction)
 {
   drivetrainInit("brake");
-  int ticks = (inches / 4) * 360;
+  int target = (inches / (4 * M_PI)) * 360;
   int lAvgTicks = 0;
   int rAvgTicks = 0;
   int avgTicks = 0;
@@ -113,22 +136,22 @@ void driveDistance(int inches, std::string direction)
   int alignErr = 0;
   float distKp = 0;
   float alignKp = 0;
-  const int SLEW = 0;
-  while(avgTicks < ticks)
+  const float SLEW = 0;//This just so happens to also be the acceleration
+  while(avgTicks < target)
   {
     lAvgTicks = abs(getAvgDriveSideTicks('l'));
     rAvgTicks = abs(getAvgDriveSideTicks('r'));
-    avgTicks = (lAvgTicks + rAvgTicks)/2;
+    avgTicks = (lAvgTicks + rAvgTicks) / 2;
 
     //Make sure we dont accelerate/decelerate too fast with slew
-    distErr = (ticks - avgTicks) * distKp;
+    distErr = (target - avgTicks) * distKp;
     if(distErr > SLEW)
     {
       distErr = SLEW;
     }
 
     //Decide wether to accelerate or decelerate
-    if(currentPower > (ticks - avgTicks))
+    if(currentPower > (target - avgTicks))
     {
       distErr = distErr * -1;
     }
@@ -169,6 +192,7 @@ void driveDistance(int inches, std::string direction)
     {
       currentPower = 0;
     }
+    delay(1);
   }
   stopAll();
 }
@@ -176,7 +200,7 @@ void driveDistance(int inches, std::string direction)
 void turnDegrees(int degrees, std::string direction)
 {
   drivetrainInit("brake");
-  int ticks = degrees * 0;//Ticks per degree
+  int target = degrees * 0;//Ticks per degree
   int lAvgTicks = 0;
   int rAvgTicks = 0;
   int avgTicks = 0;
@@ -188,21 +212,21 @@ void turnDegrees(int degrees, std::string direction)
   float distKp = 0;
   float alignKp = 0;
   const int SLEW = 0;
-  while(avgTicks < ticks)
+  while(avgTicks < target)
   {
     lAvgTicks = abs(getAvgDriveSideTicks('l'));
     rAvgTicks = abs(getAvgDriveSideTicks('r'));
-    avgTicks = (lAvgTicks + rAvgTicks)/2;
+    avgTicks = (lAvgTicks + rAvgTicks) / 2;
 
     //Make sure we dont accelerate/decelerate too fast with slew
-    distErr = (ticks - avgTicks) * distKp;
+    distErr = (target - avgTicks) * distKp;
     if(distErr > SLEW)
     {
       distErr = SLEW;
     }
 
     //Decide wether to accelerate or decelerate
-    if(currentPower > (ticks - avgTicks))
+    if(currentPower > (target - avgTicks))
     {
       distErr = distErr * -1;
     }
@@ -237,6 +261,56 @@ void turnDegrees(int degrees, std::string direction)
     //Set current power for next cycle, make sure it doesn't get too high/low
     /*As a side note, the distance(in ticks) at which deceleration starts is
       determined by the upper limit on currentPower*/
+    currentPower = currentPower + distErr;
+    if(currentPower > 200)
+    {
+      currentPower = 200;
+    }
+    else if(currentPower < 0)
+    {
+      currentPower = 0;
+    }
+  }
+}
+
+void strafeInches(int inches, char direction)
+{
+  drivetrainInit("brake");
+  int target = (inches / (4 * M_PI));
+  int ticks = 0;
+  int lAvgTicks = 0;
+  int rAvgTicks = 0;
+  int currentPower = 0;
+  int power = 0;
+  int distErr = 0;
+  int alignErr = 0;
+  float distKp = 0;
+  float alignKp = 0;
+  const int SLEW = 0;
+  while(ticks < target)
+  {
+    ticks = abs(getStrafeTicks());
+    lAvgTicks = abs(getAvgDriveSideTicks('l'));
+    rAvgTicks = abs(getAvgDriveSideTicks('r'));
+
+    distErr = (target - ticks) * distKp;
+
+    if(distErr > SLEW)
+    {
+      distErr = SLEW;
+    }
+
+    if(currentPower > (target -ticks))
+    {
+      distErr = distErr * -1;
+    }
+
+    power = currentPower + distErr;
+    if(direction == 'l')
+    {
+      power = power * -1;
+    }
+
     currentPower = currentPower + distErr;
     if(currentPower > 200)
     {
